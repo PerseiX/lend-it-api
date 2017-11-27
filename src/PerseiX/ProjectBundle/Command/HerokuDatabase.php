8f1dc0c81b0ad15f258ebb1ee78cfe0c6bb98444
PerseiX <kamilrytel2@gmail.com>
2017-11-27 21:40:16 +0100

    Db heroku config

---
+++ src/PerseiX/ProjectBundle/Command/HerokuDatabase.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace PerseiX\ProjectBundle\Command;
+
+use Composer\Script\Event;
+
+/**
+ * Class HerokuDatabase
+ * @package PerseiX\ProjectBundle\Command
+ */
+class HerokuDatabase
+{
+	public static function populateEnvironment(Event $event)
+	{
+		$url = getenv("DATABASE_URL");
+
+		if ($url) {
+			$url = parse_url($url);
+			putenv("DATABASE_HOST={$url['host']}");
+			putenv("DATABASE_USER={$url['user']}");
+			putenv("DATABASE_PASSWORD={$url['pass']}");
+			$db = substr($url['path'], 1);
+			putenv("DATABASE_NAME={$db}");
+		}
+
+		$io = $event->getIO();
+
+		$io->write("DATABASE_URL=" . getenv("DATABASE_URL"));
+	}
+}
